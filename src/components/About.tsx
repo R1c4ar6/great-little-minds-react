f5371227ad1d9edc4441636ae324c977e370b9b2
R1c4ar6 <bleaksoulless@gmail.com>
2025-02-12 20:29:35 +0100

    fixed a routing issue after deploy

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Container, Row } from 'react-bootstrap';
 import { useTranslation } from 'react-i18next';
+import aboutPic from '../assets/IMG-20220713-WA0006.jpg';
 
 const About: React.FC = () => {
     const { t } = useTranslation();
@@ -10,7 +11,7 @@ const About: React.FC = () => {
             <Container>
                 <Row className="align-items-center">
                     <div className="col-md-6">
-                        <img src="/src/assets/IMG-20220713-WA0006.jpg" className="img-fluid rounded" alt="{t('about.title')}"></img>
+                        <img src={aboutPic} className="img-fluid rounded" alt="{t('about.title')}"></img>
                     </div>
                     <div className="col-md-6">
                         <h2>{t('about.title')}</h2>
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,22 +1,47 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Navbar, Nav, Container } from 'react-bootstrap';
+import { Link, useLocation } from 'react-router-dom';
 import TranslateButton from './TranslateButton';
 import { useTranslation } from 'react-i18next';
 
 const NavbarComponent: React.FC = () => {
     const { t } = useTranslation();
+    const location = useLocation(); 
+
+    useEffect(() => {
+        const hash = location.hash; 
+        if (hash) {
+            const element = document.querySelector(hash);
+            if (element) {
+                element.scrollIntoView({ behavior: 'smooth' }); // Smooth scroll to the element
+            }
+        }
+    }, [location]); // Run this effect whenever the location changes
+
     return (
         <Navbar bg="light" expand="lg" fixed="top" className="shadow-sm">
             <Container>
-                <Navbar.Brand href="/" className="menu-title">Little Great Minds</Navbar.Brand>
+                <Navbar.Brand href='/' className="menu-title">
+                    Little Great Minds
+                </Navbar.Brand>
                 <Navbar.Toggle aria-controls="navbarNav" />
                 <Navbar.Collapse id="navbarNav" className="justify-content-end">
                     <Nav>
-                        <Nav.Link href="#about">{t('navbar.about')}</Nav.Link>
-                        <Nav.Link href="#programs">{t('navbar.programs')}</Nav.Link>
-                        <Nav.Link href="#gallery">{t('navbar.gallery')}</Nav.Link>
-                        <Nav.Link href="#testimonials">{t('navbar.testimonials')}</Nav.Link>
-                        <Nav.Link href="#contact">{t('navbar.contact')}</Nav.Link>
+                        <Nav.Link as={Link} to="/#about">
+                            {t('navbar.about')}
+                        </Nav.Link>
+                        <Nav.Link as={Link} to="/#programs">
+                            {t('navbar.programs')}
+                        </Nav.Link>
+                        <Nav.Link as={Link} to="/#gallery">
+                            {t('navbar.gallery')}
+                        </Nav.Link>
+                        <Nav.Link as={Link} to="/#testimonials">
+                            {t('navbar.testimonials')}
+                        </Nav.Link>
+                        <Nav.Link as={Link} to="/#contact">
+                            {t('navbar.contact')}
+                        </Nav.Link>
                         <TranslateButton />
                     </Nav>
                 </Navbar.Collapse>
